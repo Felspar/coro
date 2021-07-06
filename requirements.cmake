7a1824c1669bff9348228fedee7e5d9ee3f9050f
Kirit Sælensminde <kirit@kirit.com>
2021-07-06 09:43:04 +0700

    Include felspar-test before felspar-memory

--- requirements.cmake
+++ requirements.cmake
@@ -1,17 +1,6 @@
 include(FetchContent)
 
 FetchContent_Declare(
-        felspar-memory
-        GIT_REPOSITORY https://github.com/Felspar/memory.git
-        GIT_TAG main
-    )
-FetchContent_GetProperties(felspar-memory)
-if(NOT felspar-memory_POPULATED)
-    FetchContent_Populate(felspar-memory)
-    add_subdirectory(${felspar-memory_SOURCE_DIR} ${felspar-memory_BINARY_DIR})
-endif()
-
-FetchContent_Declare(
         felspar-test
         GIT_REPOSITORY https://github.com/Felspar/test.git
         GIT_TAG main
@@ -20,4 +9,15 @@ FetchContent_GetProperties(felspar-test)
 if(NOT felspar-test_POPULATED)
     FetchContent_Populate(felspar-test)
     add_subdirectory(${felspar-test_SOURCE_DIR} ${felspar-test_BINARY_DIR})
+endif()
+
+FetchContent_Declare(
+        felspar-memory
+        GIT_REPOSITORY https://github.com/Felspar/memory.git
+        GIT_TAG main
+    )
+FetchContent_GetProperties(felspar-memory)
+if(NOT felspar-memory_POPULATED)
+    FetchContent_Populate(felspar-memory)
+    add_subdirectory(${felspar-memory_SOURCE_DIR} ${felspar-memory_BINARY_DIR})
 endif()
