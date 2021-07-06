ad5f36f785bf70a63f4172e5a0cc1c06ba0c67d6
Kirit Sælensminde <kirit@kirit.com>
2021-07-06 09:44:53 +0700

    We're now also using the exceptions library.

--- requirements.cmake
+++ requirements.cmake
@@ -11,6 +11,18 @@ if(NOT felspar-test_POPULATED)
     add_subdirectory(${felspar-test_SOURCE_DIR} ${felspar-test_BINARY_DIR})
 endif()
 
+
+FetchContent_Declare(
+        felspar-exceptions
+        GIT_REPOSITORY https://github.com/Felspar/exceptions.git
+        GIT_TAG main
+    )
+FetchContent_GetProperties(felspar-exceptions)
+if(NOT felspar-memory_POPULATED)
+    FetchContent_Populate(felspar-exceptions)
+    add_subdirectory(${felspar-exceptions_SOURCE_DIR} ${felspar-exceptions_BINARY_DIR})
+endif()
+
 FetchContent_Declare(
         felspar-memory
         GIT_REPOSITORY https://github.com/Felspar/memory.git
