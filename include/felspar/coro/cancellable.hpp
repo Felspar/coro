53f70dcfa228b0e299156d49d9746b09b1834bd2
Kirit Sælensminde <kirit@kirit.com>
2021-09-08 11:33:33 +0700

    Add a cancellation wrapper for awaitables.

---
+++ include/felspar/coro/cancellable.hpp
@@ -0,0 +1,57 @@
+#pragma once
+
+
+#include <felspar/coro/coroutine.hpp>
+
+
+namespace felspar::coro {
+
+
+    /**
+     * Wait at the suspension point until resumed from an external location.
+     */
+    class cancellable {
+        coroutine_handle<> continuation = {};
+        bool signalled = false;
+
+        void resume_if_needed() {
+            if (continuation) { std::exchange(continuation, {}).resume(); }
+        }
+
+      public:
+        /// Used externally to cancel the controlled coroutine
+        void cancel() {
+            signalled = true;
+            resume_if_needed();
+        }
+
+        /// Wrap an awaitable so that an early resumption can be signalled
+        template<typename A>
+        auto signal_or(A coro_awaitable) {
+            struct awaitable {
+                A a;
+                cancellable &b;
+
+                bool await_ready() const noexcept {
+                    return b.signalled or a.await_ready();
+                }
+                auto await_suspend(coroutine_handle<> h) noexcept {
+                    /// `h` is the coroutine making use of the `cancellable`
+                    b.continuation = h;
+                    return a.await_suspend(h);
+                }
+                auto await_resume()
+                        -> decltype(std::declval<A>().await_resume()) {
+                    if (b.signalled) {
+                        return {};
+                    } else {
+                        return a.await_resume();
+                    }
+                }
+            };
+            return awaitable{std::move(coro_awaitable), *this};
+        }
+    };
+
+
+}
