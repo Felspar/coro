88198a2154c9c2ab109239e0a21ea4337c7606da
Kirit Sælensminde <kirit@felspar.com>
2022-12-17 12:50:16 +0700

    Remove incorrect copyrights

--- include/felspar/coro/coroutine.hpp
+++ include/felspar/coro/coroutine.hpp
@@ -1,11 +1,3 @@
-/**
-    Copyright 2020 Red Anchor Trading Co. Ltd.
-
-    Distributed under the Boost Software License, Version 1.0.
-    See <http://www.boost.org/LICENSE_1_0.txt>
- */
-
-
 #pragma once
 
 
